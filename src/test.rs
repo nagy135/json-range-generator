b41062da7ab81bfa93340ac464a36d802977317e
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-06-04 17:09:36 +0200

    test: prefix/suffix

--- src/test.rs
+++ src/test.rs
@@ -44,3 +44,34 @@ fn basic() {
 
     assert_eq!(format!("{}", &v), format!("{}", &desired));
 }
+
+#[test]
+fn prefix_suffix() {
+    let data = r#"
+    {
+        "before_<2-3>_after": {
+            "name#<88-90>": "John Doe"
+        }
+    }"#;
+
+    let mut v: Value = serde_json::from_str(data).unwrap();
+
+    recurse_obj(&mut v);
+
+    let desired = json!(
+        {
+            "before_2_after": {
+                "name#88": "John Doe",
+                "name#89": "John Doe",
+                "name#90": "John Doe"
+            },
+            "before_3_after": {
+                "name#88": "John Doe",
+                "name#89": "John Doe",
+                "name#90": "John Doe"
+            }
+        }
+    );
+
+    assert_eq!(format!("{}", &v), format!("{}", &desired));
+}
